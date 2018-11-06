dac5118c077af00c5efb6e85c985c3f6506af5a4
code-Einstein <36019676+code-Einstein@users.noreply.github.com>
2018-11-06 14:37:57 +0800

    Add files via upload

---
+++ 1001.cpp
@@ -0,0 +1,42 @@
+﻿/*
+1001 A+B Format （20 分）
+Calculate a+b and output the sum in standard format -- that is, the digits must be separated into groups of three by commas (unless there are less than four digits).
+
+Input Specification:
+Each input file contains one test case. Each case contains a pair of integers a and b where −10
+​6
+​​ ≤a,b≤10
+​6
+​​ . The numbers are separated by a space.
+
+Output Specification:
+For each test case, you should output the sum of a and b in one line. The sum must be written in the standard format.
+
+Sample Input:
+-1000000 9
+Sample Output:
+-999,991
+*/
+
+#include<iostream>
+#include<cstdlib>
+#include<string>
+using namespace std;
+
+int main() {
+	int a, b, c, num, tmp;
+	cin >> a >> b;
+	c = a + b;
+	if (c < 0) {
+		cout << "-";
+		c = abs(c);
+	}
+	string res = to_string(c);
+	num = res.length();
+	for (int i = 0; i < num; i++) {
+		if (i != num - 1 && i && (num - i) % 3 == 0)
+			cout << ",";
+		cout << res[i];
+	}
+	return 0;
+}
---
+++ 1002.cpp
@@ -0,0 +1,76 @@
+﻿/*
+1002 A+B for Polynomials （25 分）
+This time, you are supposed to find A+B where A and B are two polynomials.
+
+Input Specification:
+Each input file contains one test case. Each case occupies 2 lines, and each line contains the information of a polynomial:
+
+K N
+​1
+​​  a
+​N
+​1
+​​
+​​  N
+​2
+​​  a
+​N
+​2
+​​
+​​  ... N
+​K
+​​  a
+​N
+​K
+​​
+​​
+
+where K is the number of nonzero terms in the polynomial, N
+​i
+​​  and a
+​N
+​i
+​​
+​​  (i=1,2,⋯,K) are the exponents and coefficients, respectively. It is given that 1≤K≤10，0≤N
+​K
+​​ <⋯<N
+​2
+​​ <N
+​1
+​​ ≤1000.
+
+Output Specification:
+For each test case you should output the sum of A and B in one line, with the same format as the input. Notice that there must be NO extra space at the end of each line. Please be accurate to 1 decimal place.
+
+Sample Input:
+2 1 2.4 0 3.2
+2 2 1.5 1 0.5
+Sample Output:
+3 2 1.5 1 2.9 0 3.2
+*/
+
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main() {
+	double hash[1001] = { 0 };
+	int tmp1, num = 0, n;
+	double tmp2;
+	for (int i = 0; i < 2; i++) {
+		cin >> n;
+		for (int j = 0; j < n; j++) {
+			cin >> tmp1 >> tmp2;
+			hash[tmp1] += tmp2;
+		}
+	}
+	for (auto it : hash)
+		if (it) num++;
+	cout << num;
+	for (int i = 1000; i >= 0; i--) {
+		if (hash[i])
+			//printf(" %d %.1f", i, hash[i]);
+			cout << " " << fixed << setprecision(1) << i << " " << hash[i];		
+	}
+	return 0;
+}
